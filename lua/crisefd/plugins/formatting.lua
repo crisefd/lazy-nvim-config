70406dc40b55ba8ad62b796d42ae90a55f3b4075
cristhian-fuertes <cristhian.e.fuertes@gmail.com>
2024-10-15 18:51:17 -0500

    Disabling auto formatting for TS

--- lua/crisefd/plugins/formatting.lua
+++ lua/crisefd/plugins/formatting.lua
@@ -6,17 +6,17 @@ return {
 
 		conform.setup({
 			formatters_by_ft = {
-				javascript = { "prettier" },
-				typescript = { "prettier" },
+				-- javascript = { "prettier" },
+				-- typescript = { "prettier" },
 				elixir = { "mix" },
 				eelixir = { "mix" },
 				heex = { "mix" },
 				surface = { "mix" },
-				javascriptreact = { "prettier" },
-				typescriptreact = { "prettier" },
+				-- javascriptreact = { "prettier" },
+				-- typescriptreact = { "prettier" },
 				svelte = { "prettier" },
-				css = { "prettier" },
-				html = { "prettier" },
+				-- css = { "prettier" },
+				-- html = { "prettier" },
 				json = { "prettier" },
 				yaml = { "prettier" },
 				markdown = { "prettier" },
@@ -25,11 +25,11 @@ return {
 				lua = { "stylua" },
 				python = { "isort", "black" },
 			},
-			format_on_save = {
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 1000,
-			},
+			-- format_on_save = {
+			-- 	lsp_fallback = true,
+			-- 	async = false,
+			-- 	timeout_ms = 1000,
+			-- },
 		})
 
 		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
