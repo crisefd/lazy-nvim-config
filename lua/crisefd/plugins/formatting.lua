f8c42fe2adea70e2c228b700e8752f411320fc05
Chris The Weird <cristhian.e.fuertes@gmail.com>
2025-04-08 00:36:25 -0500

    adding basic go support

--- lua/crisefd/plugins/formatting.lua
+++ lua/crisefd/plugins/formatting.lua
@@ -6,6 +6,7 @@ return {
 
 		conform.setup({
 			formatters_by_ft = {
+				go = { "gofmt" },
 				javascript = { "prettier" },
 				typescript = { "prettier" },
 				elixir = { "mix" },
--- lua/crisefd/plugins/lsp/mason.lua
+++ lua/crisefd/plugins/lsp/mason.lua
@@ -27,7 +27,7 @@ return {
 		mason_lspconfig.setup({
 			-- list of servers for mason to install
 			ensure_installed = {
-				"tsserver",
+				-- "tsserver",
 				"grammarly",
 				"eslint",
 				"sqlls",
@@ -50,6 +50,8 @@ return {
 				"emmet_ls",
 				"prismals",
 				"pyright",
+				"gopls",
+				-- "delve",
 			},
 		})
 
